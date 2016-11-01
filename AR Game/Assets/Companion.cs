0cfae6506abcc541c9d5e084b3372feddd0b1b78
Muigai <muigaiunaka@gmail.com>
2016-11-01 16:49:01 -0400

    Classes for Game database UML

---
+++ AR Game/Assets/Companion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Companion : MonoBehaviour {
+	enum Type {
+		Chimera1,
+		Chimera2,
+		Chimera3,
+		Chimera4,
+		Chimera5
+	};
+	int level;
+	float humanityPoints;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
