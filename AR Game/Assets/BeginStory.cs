148cf5264a6c7bfc20c5b810a5fa225486624345
rossar19 <ari_ross19@yahoo.com>
2016-11-04 16:22:47 -0400

    Add StartGame and BeginStory scripts for start button

---
+++ AR Game/Assets/BeginStory.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BeginStory : MonoBehaviour {
+	private int counter = 0;
+
+	// Use this for initialization
+	void Start () {
+		print("Script Added!");
+
+		GameObject contBtn = GameObject.Find("Continue Btn");
+
+		Button btn = contBtn.AddComponent<Button>();
+		contBtn.AddComponent<Image>();
+		btn.onClick.AddListener(Next);
+
+		GameObject cTextGO = new GameObject("Continue Text");
+		cTextGO.transform.SetParent(contBtn.transform);
+
+	    Text contText = cTextGO.AddComponent<Text>();
+	    contText.text = "Next";
+		contText.color = Color.black;
+		contText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+		contText.transform.position = new Vector2(50, -15);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Next() {
+		counter++;
+		Text narrText = GameObject.Find("Narrative Text").GetComponent<Text>();
+		narrText.text = "You clicked next " + counter + " time(s)";
+	}
+}
---
+++ AR Game/Assets/StartGameScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartGameScript : MonoBehaviour {
+	public GameObject canvas;
+	public Button startBtn;
+	public Text btnText;
+
+	// Use this for initialization
+	void Start () {
+		btnText.text = "Start Game";
+		Button btn = startBtn.GetComponent<Button>();
+		btn.onClick.AddListener(StartGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void StartGame() {
+		btnText.text = "Game Started";
+		if (GameObject.Find("Narrative Text") == null) {
+			GameObject textGO = new GameObject("Narrative Text");
+		    textGO.transform.SetParent(canvas.transform);
+		 
+		    Text narrText = textGO.AddComponent<Text>();
+		    narrText.text = "What is life?";
+			narrText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+			// position starts at -339 and -150...using this for now to counter
+			narrText.transform.position = new Vector2(339, 50);
+
+			GameObject contBtn = new GameObject("Continue Btn");
+			contBtn.transform.SetParent(canvas.transform);
+			contBtn.AddComponent<BeginStory>();
+
+			Destroy(GameObject.Find("Button"));
+		}
+		else {
+			print("Text already exists!");
+		}
+	}
+}
