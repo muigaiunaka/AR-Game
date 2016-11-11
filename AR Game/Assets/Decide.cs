44da8a6786ee7eb04aa6b4153f32418a40994f46
Jesse Oberstein <oberstein.j@husky.neu.edu>
2016-11-11 16:02:36 -0500

    personality-test: added prefabs for decision buttons and questions. Using xml from a previous project as an example of a question tree.

---
+++ AR Game/Assets/Decide.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Xml;
+
+public class Decide : MonoBehaviour {
+	private ArrayList nextDecisions;
+	private Button buttonObject;
+	private GameObject canvas;
+	public Decision decision;
+//	public Transform decisionPrefab;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.Find ("Canvas");
+		this.buttonObject = gameObject.GetComponent<Button>();
+		this.buttonObject.onClick.AddListener (decide);
+		this.nextDecisions = new ArrayList();
+
+	}
+
+	void decide() {
+//		Text buttonText = buttonObject.GetComponentInChildren<Text> ();
+//		string decisionLetter = buttonText.text.Split (new char[] { ')' }) [0];
+	
+		PersonalityTest personalityTest = canvas.GetComponent<PersonalityTest>();
+		Debug.Log (personalityTest.currentDecisions.Count);
+
+		Destroy (personalityTest.currentQuestion);
+		foreach (GameObject option in personalityTest.currentDecisions) {
+			Destroy (option);
+		}
+
+		personalityTest.currentDecisions.Clear ();
+		personalityTest.parseQuestion (decision.getNextQuestion ());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Touch
+		if (Input.touchCount > 0 && (TouchPhase.Began == Input.GetTouch (0).phase)) {
+			Debug.Log ("Touched!");
+		}
+
+
+		/*
+		Began	A finger touched the screen.
+		Moved	A finger moved on the screen.
+		Stationary	A finger is touching the screen but hasn't moved.
+		Ended	A finger was lifted from the screen. This is the final phase of a touch.
+		Canceled	The system cancelled tracking for the touch.
+		*/
+	}
+}
+
+
+public class Decision {
+	private string option;
+	private XmlNodeList nextQuestions;
+
+	public Decision(string option) {
+		this.option = option;
+		this.nextQuestions = null;
+	}
+
+	public Decision(string option, XmlNodeList nextQuestions) {
+		this.option = option;
+		this.nextQuestions = nextQuestions;
+	}
+
+	public string getOptionText() {
+		return this.option;
+	}
+
+	public void setOptionText(string option) {
+		this.option = option;
+	}
+
+	public XmlNodeList getNextQuestion() {
+		return this.nextQuestions;
+	}
+
+	public void setNextQuestion(XmlNodeList nextQuestions) {
+		this.nextQuestions = nextQuestions;
+	}
+
+	override
+	public string ToString() {
+		return this.option;
+	}
+}
---
+++ AR Game/Assets/PersonalityTest.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class PersonalityTest : MonoBehaviour {
+	public static char[] DECISION_LETTERS = new char[5]{'a', 'b', 'c', 'd', 'e'};
+	private static string XML_FILE = "FairyTailMagicText.xml";
+
+	public GameObject decisionPrefab;
+	public GameObject questionPrefab;
+	public GameObject currentQuestion;
+	public GameObject canvas;
+
+	private int questionCount;
+
+	public HashSet<GameObject> currentDecisions;
+
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.Find ("Canvas");
+		resetCanvasTransform ();
+
+		questionCount = 1;
+		currentDecisions = new HashSet<GameObject> ();
+
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.Load(XML_FILE);
+		XmlNodeList personalityTest = xmlDoc.GetElementsByTagName("story");
+		XmlNodeList firstQuestion = personalityTest[0].ChildNodes;
+		parseQuestion (firstQuestion);
+	}
+
+	public void parseQuestion(XmlNodeList question) {
+		int counter = 45;
+
+		foreach (XmlNode item in question) {
+			string tag = item.Name;
+			resetCanvasTransform ();
+
+			switch (item.Name) {
+
+				case "description":
+					Debug.Log ("Description: " + item.InnerText);
+					break;
+
+				case "prompt":
+					if ("n".Equals (item.Attributes ["end"].Value)) {
+						GameObject questionObj = Instantiate (questionPrefab, canvas.transform) as GameObject;
+						questionObj.GetComponent<Text> ().text = (questionCount.ToString () + ") " + item.InnerText);
+						questionObj.transform.position = new Vector3 (-40, 80, 1);
+						currentQuestion = questionObj;
+						questionCount++;
+					}
+					break;
+
+				case "opt":
+					string text = item.SelectSingleNode ("decision").FirstChild.Value;
+					Decision decision = new Decision (text, item.ChildNodes);
+					GameObject decisionObj = Instantiate (decisionPrefab, canvas.transform) as GameObject;
+					decisionObj.transform.position = new Vector3(0, counter, 0);
+					decisionObj.GetComponentInChildren<Text> ().text = "  " + (DECISION_LETTERS[currentDecisions.Count]) + ") " + decision.getOptionText ();
+					decisionObj.GetComponent<Decide> ().decision = decision;
+					currentDecisions.Add (decisionObj);
+					counter -= 40;
+					break;
+
+				case "ending":
+					GameObject endingObj = Instantiate (questionPrefab, canvas.transform) as GameObject;
+					endingObj.GetComponent<Text> ().text = item.InnerText;
+					endingObj.transform.position = new Vector3 ();
+					return;
+			}
+		}
+	}
+
+	void resetCanvasTransform() {
+		canvas.transform.position = new Vector3 (0, 0, 0);
+		canvas.transform.rotation = Quaternion.identity;
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
